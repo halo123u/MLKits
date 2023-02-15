63a4036b75b65b38272ba338b853a235f734ac86
Oswaldo Almazo <oswaldo@justworkslabs.com>
2023-02-14 23:35:39 -0500

    attempt to improve accuracy and add console to see results.

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,6 @@
 const outputs = []
-const predictionPoint = 300;
-const k = 3;
-function distance(point) {
-  return Math.abs(point - predictionPoint)
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB)
 }
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -12,9 +10,20 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
-  const test = _.chain(outputs)
-  .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 100
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize)
+  
+  _.range(1, 20).forEach(k => {
+    const accuracy = _.chain(testSet).filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+    .size().divide(testSetSize).value()
+  
+    console.log('For k of ', k, ' Accuracy is', accuracy)
+  })
+}
+
+function knn(data, point, k){
+  return _.chain(data)
+  .map(row => [distance(row[0], point), row[3]])
   .sortBy(row => row[0])
   .slice(0, k)
   .countBy(row => row[1])
@@ -23,6 +32,12 @@ function runAnalysis() {
   .last()
   .first()
   .parseInt().value()
-  console.log(test)
 }
 
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data)
+  const testSet = _.slice(shuffled, 0, testCount)
+  const trainingSet = _.slice(shuffled, testCount)
+
+  return [testSet, trainingSet]
+}
